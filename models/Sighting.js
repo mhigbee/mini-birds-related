e5b0a86394c1825bf3a060b12690464f60bdf54d
Matt Higbee <mhigbee@hotmail.com>
2016-07-09 10:21:27 -0600

    saving progress

--- models/Sighting.js
+++ models/Sighting.js
@@ -3,24 +3,7 @@ var Schema = mongoose.Schema;
 
 var sightingSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User'},
-  bird: [{
-    name: { type: String, lowercase: true },
-    order: { type: String, lowercase: true, maxlength: 20 },
-    status: {
-      type: String,
-      lowercase: true,
-      enum: [
-        'extinct',
-        'extinct in the wild',
-        'critically endangered',
-        'endangered',
-        'vulnerable',
-        'near threatened',
-        'conservation dependent',
-        'least concern'
-      ]
-    }
-  }],
+  bird: { type: Schema.Types.ObjectId, ref: 'Bird'},
   confirmed: { type: Boolean, default: false },
   numberSeen: { type: Number, min: 1 }
 });
